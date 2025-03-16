51aec20135f95944539b97ca825ddfd9a19ef5a7
junyeong <whwnsdud4592@naver.com>
2025-03-16 15:28:46 +0900

    docs(README) TDD과정 추가

--- src/main/java/Controller/Main.java
+++ src/main/java/Controller/Main.java
@@ -13,7 +13,7 @@ public class Main {
   public static void main(String[] args){
     List<String> coordList = inputView.readCoordinate();
     Points points = new Points(coordList);
-
+    System.out.print(points.getTwoPointDistance());
   }
 
 }
