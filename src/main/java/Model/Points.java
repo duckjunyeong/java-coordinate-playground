6acf79700605350e08bf40a55836bc24a2f498f8
junyeong <whwnsdud4592@naver.com>
2025-03-17 16:20:33 +0900

    feat(Points) calcuRectangleExtent 함수 구현 (직사각형의 넓이를 구한다)

--- src/main/java/Model/Points.java
+++ src/main/java/Model/Points.java
@@ -1,7 +1,7 @@
 package Model;
 
 import utils.CoordCalcualtor;
-
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,5 +20,32 @@ public class Points {
 
   public List<Point> getPointList(){
     return pointList;
+  }
+
+  public int calcuRectangleExtent() {
+    Collections.sort(pointList);
+    Point pos1 = pointList.get(0);
+    Point pos2 = pointList.get(1);
+    Point pos3 = pointList.get(2);
+    Point pos4 = pointList.get(3);
+
+    if (isSamePosOfX(pos1, pos2) && isSamePosOfX(pos3, pos4)) {
+      if (getColSize(pos1, pos2) == getColSize(pos2, pos3)) {
+        return getColSize(pos1, pos2) * getRowSize(pos1, pos3);
+      }
+    }
+    throw new IllegalArgumentException("직사각형이 아닙니다.");
+  }
+
+  public int getRowSize(Point pos1, Point pos2){
+    return Math.abs(pos1.getX() - pos2.getX());
+  }
+
+  public int getColSize(Point pos1, Point pos2){
+    return Math.abs(pos1.getY() - pos2.getY());
+  }
+
+  public boolean isSamePosOfX(Point pos1, Point pos2){
+    return pos1.getX() == pos2.getX();
   }
 }
--- src/test/java/PointsTest.java
+++ src/test/java/PointsTest.java
@@ -4,11 +4,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PointsTest {
 
@@ -17,8 +15,23 @@ public class PointsTest {
   void is_generate_object(){
     Point coord1 = new Point(4, 9);
     Point coord2 = new Point(14, 19);
-
     Points points = new Points(Arrays.asList("4,9", "14,19"));
     assertThat(points.getPointList()).isEqualTo(Arrays.asList(coord1, coord2));
+  }
+
+  @Test
+  @DisplayName("pointList에 존재하는 좌표값들이 직사각형을 이루는지 판단하는가")
+  void calcuRectangleExtent_test(){
+    Points points = new Points(Arrays.asList("10,10", "22,10", "22,18", "10,18"));
+    assertThat(points.calcuRectangleExtent()).isEqualTo(96);
+  }
+
+  @Test
+  @DisplayName("pointList에 존재하는 좌표값들이 직사각형이 아니라면 예외를 발생시키는가")
+  void calcuRectangleExtent_test_throwException(){
+    Points points = new Points(Arrays.asList("10,9", "22,10", "22,18", "10,18"));
+    assertThrows(IllegalArgumentException.class, () -> {
+      points.calcuRectangleExtent();
+    });
   }
 }
