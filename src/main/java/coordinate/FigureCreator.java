b077ff43b8f53bef9ec471fdaa1eccd3c36e57d6
junyeong <whwnsdud4592@naver.com>
2025-03-19 13:28:53 +0900

    feat 시도를 했으나 어떻게 구현할 지 감을 잡지 못 함

--- src/main/java/coordinate/FigureCreator.java
+++ src/main/java/coordinate/FigureCreator.java
@@ -5,3 +5,14 @@ import java.util.List;
 public interface FigureCreator {
     Figure create(List<Point> points);
 }
+
+
+
+// 이 create가 왜 있을 까 결국엔 points들의 size를 보고
+// 어떤 도형을 만들지 판단하는 메서드거든
+// points의 size값에 따라서 도형을 만들어 주는 추상화 메서드..
+
+// 이 기능을 어디에 넣는게 좋겠어 도형을 만든다라고 했을 때
+// 당연히 FigureFactory 클래스가 좋을 것 같애
+//
+
--- src/main/java/coordinate/FigureFactory.java
+++ src/main/java/coordinate/FigureFactory.java
@@ -2,9 +2,14 @@ package coordinate;
 
 import java.util.List;
 
-public class FigureFactory {
+public class FigureFactory implements FigureCreator{
     static Figure getInstance(List<Point> points) {
-        if (points.size() == Line.LINE_POINT_SIZE) {
+        Figure figure = create
+    }
+
+    @Override
+    public Figure create(List<Point> points){
+        if (points.size() == Line.LINE_POINT_SIZE){
             return new Line(points);
         }
 
@@ -15,7 +20,12 @@ public class FigureFactory {
         if (points.size() == Rectangle.RECTANGLE_POINT_SIZE) {
             return new Rectangle(points);
         }
-
         throw new IllegalArgumentException("유효하지 않은 도형입니다.");
     }
 }
+
+
+// Map 컬렉션은 뭘까 키와 값을 가지는 키-쌍구조 자료구조인데..
+// 내가 키를 알고 있으면 값을 알게 되는, 내가 도형의 이름을 넣으면 해당 객체가 나오는거야?
+// 아니지 이걸 반환해주면 Figure 클래스니깐 getName()으로 key에 접근해 g
+// 인스턴스를 가져올 수 있는거 아니야?
